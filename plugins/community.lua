49b51d718e7c2d2c8d5479eb2f2d7d19ce4bd8c2
kazuyabr <sulivan.lineage2@gmail.com>
2024-03-09 16:25:45 -0300

    mais plugins =D

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,5 +1,22 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.colorscheme.kanagawa-nvim" }
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.bars-and-lines.lualine-nvim"},
+  { import = "astrocommunity.editing-support.dial-nvim"},
+  { import = "astrocommunity.editing-support.cloak-nvim"},
+  { import = "astrocommunity.editing-support.comment-box-nvim"},
+  { import = "astrocommunity.editing-support.text-case-nvim"},
+  { import = "astrocommunity.editing-support.true-zen-nvim"},
+  { import = "astrocommunity.editing-support.todo-comments-nvim"},
+  { import = "astrocommunity.editing-support.telescope-undo-nvim"},
+  { import = "astrocommunity.editing-support.suda-vim"},
+  { import = "astrocommunity.editing-support.refactoring-nvim"},
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim"},
+  { import = "astrocommunity.editing-support.nvim-treesitter-endwise"},
+  { import = "astrocommunity.editing-support.nvim-regexplainer"},
+  { import = "astrocommunity.editing-support.neogen"},
+  { import = "astrocommunity.editing-support.hypersonic-nvim"},
+  { import = "astrocommunity.editing-support.chatgpt-nvim"},
 }
+
